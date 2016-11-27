e257cbc4cbf384992ed8ac1c59cd60a8970d4815
Tamaghna Banerjee <tamaghna@anudip.org>
2016-11-27 11:31:14 +0530

    Changes slot for addition

--- resources/views/slots/list.blade.php
+++ resources/views/slots/list.blade.php
@@ -8,78 +8,89 @@
 	<div class="row left-align">
 		<ul class="collection">
 			<li class="collection-item avatar">
-				<i class="material-icons circle blue lighten-1">today</i>
-				<span class="title black-text">26th October | 08:00 PM - 12:00 AM</span>
+				<i class="material-icons circle grey lighten-1">today</i>
+				<span class="title black-text slot-details">26th October | 08:00 PM - 12:00 AM</span>
 				<p class="blue-grey-text text-darken-4">Material icons are beautifully crafted, delightful, and easy to use in your web
-				<br><br/>
-					<a class="orange white-text mod-action" href="#!">
+					<br><br/>
+					<a class="blue accent-3 white-text mod-action" href="#!">
 						<i class="material-icons tiny relative">edit</i>Change
 					</a>
-					<a class="red accent-3 white-text margin-left-0-5x mod-action" href="#!">
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">delete</i> Trash
-	
-					<a class="teal lighten-1 white-text margin-left-0-5x mod-action" href="#!">
+					</a>
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">loop</i> Repeat
 					</a>
-					<a class="deep-purple accent-3 white-text margin-left-0-5x mod-action" href="#!">
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">compare_arrows</i> Swap Request
 					</a>
 				</p>
 			</li>
 			<li class="collection-item avatar">
-				<i class="material-icons circle blue lighten-1">today</i>
-				<span class="title black-text">26th October | 08:00 PM - 12:00 AM</span>
+				<i class="material-icons circle grey lighten-1">today</i>
+				<span class="title black-text slot-details">26th October | 08:00 PM - 12:00 AM
+					<i class="relative material-icons green-text text-accent-4">done</i>
+				</span>
 				<p class="blue-grey-text text-darken-4">Material icons are beautifully crafted, delightful, and easy to use in your web
-				<br><br/>
-					<a class="orange white-text mod-action" href="#!">
+					<br><br/>
+					<a class="blue accent-3 white-text mod-action" href="#!">
 						<i class="material-icons tiny relative">edit</i>Change
 					</a>
-					<a class="red accent-3 white-text margin-left-0-5x mod-action" href="#!">
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">delete</i> Trash
-	
-					<a class="teal lighten-1 white-text margin-left-0-5x mod-action" href="#!">
+					</a>
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">loop</i> Repeat
 					</a>
 				</p>
 			</li>
 			<li class="collection-item avatar">
-				<i class="material-icons circle blue lighten-1">today</i>
-				<span class="title black-text">26th October | 08:00 PM - 12:00 AM</span>
+				<i class="material-icons circle grey lighten-1">today</i>
+				<span class="title black-text slot-details">26th October | 08:00 PM - 12:00 AM
+					<i class="relative material-icons green-text text-accent-4">done</i>
+				</span>
 				<p class="blue-grey-text text-darken-4">Material icons are beautifully crafted, delightful, and easy to use in your web
-				<br><br/>
-					<a class="orange white-text mod-action" href="#!">
+					<br><br/>
+					<a class="blue accent-3 white-text mod-action" href="#!">
 						<i class="material-icons tiny relative">edit</i>Change
 					</a>
-					<a class="red accent-3 white-text margin-left-0-5x mod-action" href="#!">
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">delete</i> Trash
-	
-					<a class="teal lighten-1 white-text margin-left-0-5x mod-action" href="#!">
+					</a>
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">loop</i> Repeat
 					</a>
 				</p>
 
 				<a href="#!" class="secondary-content">
-					<i class="material-icons green-text text-accent-3 tooltipped" data-position="top" data-delay="50" data-tooltip="This slot is used and undo not possible">done</i>
 					<i class="material-icons red-text tooltipped" data-position="top" data-delay="50" data-tooltip="This slot is reserved on prior basis">error</i>
 				</a>
 			</li>
 			<li class="collection-item avatar">
-				<i class="material-icons circle blue lighten-1">today</i>
-				<span class="title black-text">26th October | 08:00 PM - 12:00 AM</span>
-				<p class="blue-grey-text text-darken-4">Material icons are beautifully crafted, delightful, and easy to use in your web
-				<br><br/>
-					<a class="orange white-text mod-action" href="#!">
+				<i class="material-icons circle grey lighten-1">today</i>
+				<span class="title black-text slot-details">26th October | 08:00 PM - 12:00 AM
+					<i class="relative material-icons green-text text-accent-4">done</i>
+				</span>
+				<p class="blue-grey-text text-darken-4 slot-desc">Material icons are beautifully crafted, delightful, and easy to use in your web
+					<br><br/>
+					<a class="blue accent-3 white-text mod-action" href="#!">
 						<i class="material-icons tiny relative">edit</i>Change
 					</a>
-					<a class="red accent-3 white-text margin-left-0-5x mod-action" href="#!">
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">delete</i> Trash
-	
-					<a class="teal lighten-1 white-text margin-left-0-5x mod-action" href="#!">
+					</a>
+					<a class="blue accent-3 white-text margin-left-0-5x mod-action" href="#!">
 						<i class="material-icons tiny relative">loop</i> Repeat
 					</a>
 				</p>
 			</li>
 		</ul>
 	</div>
+	<div class="fixed-action-btn horizontal slot-add">
+		<a class="btn-floating btn-large red accent-3" href="{{URL('slot/new')}}">
+			<i class="large material-icons">mode_edit</i>
+		</a>
+	</div>
 </main>
+
 @endsection
